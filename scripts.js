9ab84f5f9f1961706564e446842e4afec4183200
Teacher <teacher-02@ah4tjq1j0q6w2.78b420.nycboe.org>
2024-03-04 12:29:49 -0500

    Fix video

---
+++ scripts.js
@@ -0,0 +1,17 @@
+const canvas = document.querySelector('.photo');
+const video =document.querySelector(".player");
+const ctx = canvas.getContext("2d");
+const strip = document.querySelector(".strip");
+const snap = document.querySelector(".snap");
+
+function getVideo() {
+  navigator.mediaDevices .getUserMedia({video: true, audio: false })
+   .then(localMediaStream => {
+    console.log(localMediaStream); /* this accesses the user video data so we can use it on the page) */
+     video.srcObject = localMediaStream;
+     video.play();
+})
+  .catch(err => {
+     console.error(`OH NO!!!`, err);
+   });
+}
